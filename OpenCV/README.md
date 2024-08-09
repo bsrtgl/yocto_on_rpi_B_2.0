4a613c2e11bae8c6acd05d16e55fe5fc61f5f69c
Creciunel <60750488+Creciunel@users.noreply.github.com>
2024-08-09 16:34:03 +0300

    update instruction

--- OpenCV/README.md
+++ OpenCV/README.md
@@ -7,5 +7,7 @@ Segmentate objet in video using openCV. It can be used for image processing.
 * Python 3.8.10
 * OpenCV 4.5.5
 * Raspberry Pi camera v2.1 https://www.raspberrypi.com/documentation/accessories/camera.html
+* Tftpd64 DHCP server
+* RealVNC viewer
 
 **Steps:**
--- OpenCV/setup.sh
+++ OpenCV/setup.sh
@@ -1,1 +1,3 @@
 sudo apt-get update && sudo apt-get install python3-pip
+
+sudo apt-get update && sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1
